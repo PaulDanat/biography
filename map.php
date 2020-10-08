9500f844a98fb82f7fce49256e3c9329ec7aa9f9
rinnan19 <39804238+rinnan19@users.noreply.github.com>
2020-10-08 17:29:24 +0100

    Initial commit

---
+++ map.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/map.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+
+<body>
+    <div class="col-md-12" id="nav">
+        <div class="col-md-6">
+            <font id="name"><b>My Bio</b></font>
+        </div>
+        <div class="col-md-2"></div>
+        <div class="col-md-1">
+            <p class="nav"><a href="home.html"><b> Home</b></a></p>
+        </div>
+        <div class="col-md-1">
+            <p class="nav"><a href="contact.html"><b> Contact</b></a></p>
+        </div>
+        <div class="col-md-1">
+            <p class="nav"><a href="map.html"><b> Location </b></a></p>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+    <div class="mapouter" class="col-md-12">
+        <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas"
+                src="https://maps.google.com/maps?q=wootlab&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+                scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2torrentz.net">torrentz2
+                malayalam movies</a>
+        </div>
+    </div>
+    <div class="col-md-12" id="foot">
+        <div class="col-md-6">
+            <p id="rights">
+                @ 2020 MY BIO. ALL RIGHTS RESERVED.
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ test.php
@@ -0,0 +1,23 @@
+<div class="col-md-12" id="foot">
+    <div class="col-md-5">
+        <P id="rights">
+            @ 2020 MY BIO. ALL RIGHTS RESERVED.
+        </P>
+    </div>
+    <div class="col-md-6">
+        <p id="icon">
+            <br><a href="https://www.Twitter'.com/">
+                <img alt="twitter" src="media/twitter.png" >
+            </a> &nbsp;
+            <a href="https://www.Facebook'.com/">
+                <img alt="Facebook" src="media/face.png" width="40px" height="30px">
+            </a> &nbsp;
+            <a href="https://www.Linked'.com/">
+                <img alt="Linked" src="media/linked.png" width="40px" height="30px">
+            </a> &nbsp;
+            <a href="https://www.Mail'.com/">
+                <img alt="Mail" src="media/mail.png" width="40px" height="30px">
+            </a>
+        </p>
+    </div>
+</div>
